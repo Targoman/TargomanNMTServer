f722b4c4c3be6e5de871271f7519437e3381fe02
Behrooz Vedadian <vedadian@gmail.com>
2022-11-09 11:13:44 +0330

    FIXES PREVIOUS MISTAKE
    -Completes the incomplete run_nmt_server.sh script
    -Removes ununsed symlink to TargomanNMTServer executable

--- docker/run_nmt_server.sh
+++ docker/run_nmt_server.sh
@@ -6,45 +6,51 @@ function tolower() {
   echo $1 | tr '[:upper:]' '[:lower:]'
 }
 
-function locate_file() {
-  local FILE=$(find $MODEL_BASE_FOLDER/ -name $1 2>/dev/null)
-  echo $FILE
+function locate() {
+  echo $(find $MODEL_BASE_FOLDER/ -name $1 2>/dev/null | head)
 }
 
-function locate_vocabs() {
-}
+MODEL_BASE_FOLDER="$1"
+SERVER_NAME="$2"
+SRC_LANG_ID=$(tolower "$3")
+TGT_LANG_ID=$(tolower "$4")
 
-SERVER_NAME="$1"
-SRC_LANG_ID="$2"
-TRG_LANG_ID="$3"
+SHORTLIST_FILE=$(locate "lex.${SRC_LANG_ID}${TGT_LANG_ID}.bin")
 
-MODEL_BASE_FOLDER=$(find . -name ${SRC_LANG_ID}2${TRG_LANG_ID} 2>/dev/null)
-if [ -z $MODEL_BASE_FOLDER ]; then
-  echo "Error locating the translation model." 1>&2
+SRC_SPM_FILE=$(locate "${SRC_LANG_ID}.spm")
+TGT_SPM_FILE=$(locate "${TGT_LANG_ID}.spm")
+CMN_SPM_FILE=$(locate "*.spm")
+
+SRC_BPE_FILE=$(locate "${SRC_LANG_ID}.bpe")
+TGT_BPE_FILE=$(locate "${TGT_LANG_ID}.bpe")
+CMN_BPE_FILE=$(locate "*.bpe")
+
+SRC_VCB_FILE=$(locate "vocab.${SRC_LANG_ID}.yml")
+TGT_VCB_FILE=$(locate "vocab.${TGT_LANG_ID}.yml")
+CMB_VCB_FILE=$(locate "vocab.yml")
+
+MODEL_FILE=$(find $MODEL_BASE_FOLDER -name "model*.bin" 2>/dev/null)
+INT_SHIFT_ALPHA_PARAM=$(egrep ".*alphas.bin" <<< "$MODEL_FILE" >/dev/null 2>& 1 && echo "--int8shiftAlpha")
+
+if [ -n "$SRC_SPM_FILE" -a -n "$TGT_SPM_FILE" ]; then
+  VOCAB_PARAMS="-v $SRC_SPM_FILE $TGT_SPM_FILE"
+elif [ -n "$CMN_SPM_FILE" ]; then
+  VOCAB_PARAMS="-v $CMN_SPM_FILE $CMN_SPM_FILE"
+elif [ -n "$SRC_VCB_FILE" -a -n "$TGT_VCB_FILE" ]; then
+  VOCAB_PARAMS="-v $SRC_VCB_FILE $TGT_VCB_FILE"
+elif [ -n "$CMB_VCB_FILE" ]; then
+  VOCAB_PARAMS="-v $CMB_VCB_FILE $CMB_VCB_FILE"
+else
+  echo "Error locating vocabulary files." 1>&2
   exit 1
 fi
 
-SHORTLIST_FILE=$(find $MODEL_BASE_FOLDER -name "lex*.bin" 2>/dev/null)
-MODEL_FILE=$(find $MODEL_BASE_FOLDER -name "model*.bin" 2>/dev/null)
-FIRST_SPM_FILE=$(find $MODEL_BASE_FOLDER -name "*.spm" 2>/dev/null | head)
-FIRST_BPE_FILE=$(find $MODEL_BASE_FOLDER -name "*.bpe" 2>/dev/null | head)
-if [ -r $MODEL_BASE_FOLDER/$(tolower ${SRC_LANG_ID}).spm ] && [ -r $MODEL_BASE_FOLDER/$(tolower ${TRG_LANG_ID}).spm ]; then
-  VOCAB_PARAMS="-v $MODEL_BASE_FOLDER/$(tolower ${SRC_LANG_ID}).spm $MODEL_BASE_FOLDER/$(tolower ${TRG_LANG_ID}).spm"
-elif [ -r $MODEL_BASE_FOLDER/$(tolower ${SRC_LANG_ID}).bpe ] && [ -r $MODEL_BASE_FOLDER/$(tolower ${TRG_LANG_ID}).bpe ]; then
-  VOCAB_PARAMS="--bpe_file $MODEL_BASE_FOLDER/$(tolower ${SRC_LANG_ID}).bpe $MODEL_BASE_FOLDER/$(tolower ${TRG_LANG_ID}).bpe -v $MODEL_BASE_FOLDER/vocab.$(tolower ${SRC_LANG_ID}).yml $MODEL_BASE_FOLDER/vocab.$(tolower ${TRG_LANG_ID}).yml"
-elif [ ! -z "$FIRST_SPM_FILE" ]; then
-  VOCAB_PARAMS="-v $FIRST_SPM_FILE $FIRST_SPM_FILE"
-elif [ ! -z "$FIRST_BPE_FILE" ]; then
-  VOCAB_PARAMS="--bpe_file $FIRST_BPE_FILE -v $MODEL_BASE_FOLDER/vocab.$(tolower ${SRC_LANG_ID}).yml $MODEL_BASE_FOLDER/vocab.$(tolower ${TRG_LANG_ID}).yml"
-else
-  VOCAB_PARAMS="-v $MODEL_BASE_FOLDER/vocab.$(tolower ${SRC_LANG_ID}).yml $MODEL_BASE_FOLDER/vocab.$(tolower ${TRG_LANG_ID}).yml"
+BPE_PARAMS=""
+if [ -n "$SRC_BPE_FILE" -a -n "$TGT_BPE_FILE" ]; then
+  BPE_PARAMS="--bpe_file $SRC_BPE_FILE $TGT_BPE_FILE"
+elif [ -n "$CMN_BPE_FILE" ]; then
+  BPE_PARAMS="--bpe_file $CMN_BPE_FILE"
 fi
-INT_SHIFT_ALPHA_PARAM=$(egrep ".*alphas.bin" <<< "$MODEL_FILE" >/dev/null 2>& 1 && echo "--int8shiftAlpha")
 
-echo SHORTLIST_FILE=$SHORTLIST_FILE
-echo MODEL_FILE=$MODEL_FILE
-echo VOCAB_PARAMS=$VOCAB_PARAMS
-echo INT_SHIFT_ALPHA_PARAM=$INT_SHIFT_ALPHA_PARAM
-echo $SCRIPT_DIR/TargomanNMTServer --server_name $SERVER_NAME --shortlist $SHORTLIST_FILE -m $MODEL_FILE $VOCAB_PARAMS $INT_SHIFT_ALPHA
-$SCRIPT_DIR/TargomanNMTServer --server_name $SERVER_NAME --shortlist $SHORTLIST_FILE -m $MODEL_FILE $VOCAB_PARAMS $INT_SHIFT_ALPHA
-
+echo $SCRIPT_DIR/TargomanNMTServer --server_name $SERVER_NAME --shortlist $SHORTLIST_FILE -m $MODEL_FILE $BPE_PARAMS $VOCAB_PARAMS $INT_SHIFT_ALPHA_PARAM
+$SCRIPT_DIR/TargomanNMTServer --server_name $SERVER_NAME --shortlist $SHORTLIST_FILE -m $MODEL_FILE $BPE_PARAMS $VOCAB_PARAMS $INT_SHIFT_ALPHA_PARAM
